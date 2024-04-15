9d68e4b1c41c254d95bfa1c047bee3a3f709ede5
Jaya prakash <113834605+JAYAPRAKASHMM@users.noreply.github.com>
2024-04-15 09:50:42 +0530

    Update psubscriber.js

--- psubscriber.js
+++ psubscriber.js
@@ -3,6 +3,5 @@ const subscriber = new IoRedis();
 subscriber.psubscribe("*Time");
 console.log("Receiving started");
 subscriber.on("pmessage", (pattern, channel, message) => {
-        const currentTime = new Date().toLocaleString();
         console.log(`received a message from ${channel}: ${message}`);
 });
